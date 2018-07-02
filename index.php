21f70a63d6cfd24b4d5e6872247693bc63a52420
jclejeune <38323623+jclejeune@users.noreply.github.com>
2018-07-02 23:05:38 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Le Campus des Mureaux</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <h1>Bienvenue sur le site du Campus des mureaux</h1>
+        <p>nav</p>
+    </header>
+        <section>
+            <p>contenu</p>
+        </section>
+    <footer>
+        <p>WF3 copyright 2018</p>
+    </footer>
+
+    
+    </body>
+</html>
